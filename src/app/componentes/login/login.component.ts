890fcea49db280d20d8c366dffd84a69bd465ef4
ceavalos <cipotex@hotmail.com>
2022-05-23 17:41:47 -0600

    1 file modified

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  usuario: String = '';
+  clave: String = '';
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    console.log('Usuario = ' + this.usuario + ' clave= ' + this.clave);
   }
-
 }
