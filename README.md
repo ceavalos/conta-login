2b13bac033a4699371231229c9e452701fdab9a7
ceavalos <cipotex@hotmail.com>
2022-05-23 17:22:24 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-dhpvzp-w946u1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-dhpvzp-w946u1)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmpresaComponent } from './componentes/empresa/empresa.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
+import { PartidasComponent } from './componentes/partidas/partidas.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'empresa', component: EmpresaComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: 'partida', component: PartidasComponent },
+  { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
